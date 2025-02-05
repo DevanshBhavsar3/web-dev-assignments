1dc3e3d57911dc101827241b97158b00b12c2115
Devansh Bhavsar <devanshbhavsar2006@gmail.com>
2025-02-05 20:57:39 +0530

    updated url

--- week-17/src/config.ts
+++ week-17/src/config.ts
@@ -1,2 +1,1 @@
-export const DB_URL =
-  "postgresql://neondb_owner:npg_Aq0oad8tkXjO@ep-weathered-feather-a4wj1qp6-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require";
+export const DB_URL = "your_postgres_url";
